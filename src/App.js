800296de5a78b9c05def4ed7f0ee0e6199fb1795
Suzanne Hunter <sue_huntr@yahoo.com>
2019-04-01 18:17:13 -0400

    images in grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SaladCard from "./components/SaladCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import saladstuff from "./saladstuff.json";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    saladstuff
+  };
+
+  removePoints = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const saladstuff = this.state.saladstuff.filter(saladstuff => saladstuff.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ saladstuff });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>Salad Ingredient List</Title>
+        {this.state.saladstuff.map(saladstuff => (
+          <SaladCard
+            removePoints={this.removePoints}
+            id={saladstuff.id}
+            image={saladstuff.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
