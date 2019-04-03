c4cdd08a848785da1173268895daab82ad98539f
Suzanne Hunter <sue_huntr@yahoo.com>
2019-04-03 18:23:00 -0400

    before build

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import SaladCard from "./components/SaladCard";
 import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import InfoHeader from "./components/InfoHeader";
-// import Counter from "./components/Counter";
 import saladstuff from "./saladstuff.json";
-
-//THINGS I NEED TO DO
-//Counter
-//put ID in new array
-//randomly sort the number/id
-//state is one step behind (LATER)
-
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
@@ -23,86 +15,48 @@ class App extends Component {
     savescore: 0
   };
 
+  shuffleArray = (saladstuff) => {
+    for (let i = saladstuff.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        let temp = saladstuff[i];
+        saladstuff[i] = saladstuff[j];
+        saladstuff[j] = temp;
+    }
+}
+
 
   handleClick = (id) => {
-//ONFIRST CLICK SET CURRENT SCORE TO ONE
+  //ONFIRST CLICK SET CURRENT SCORE TO ONE
+  if(this.state.count === this.state.score){
+    this.setState({ 
+      count: this.state.count + 1,
+      score: this.state.score + 1 });
+    } else if (this.state.count < this.state.score){
+      this.setState({ 
+        count: this.state.count + 1,
+        score: this.state.score});
+      } 
 
-  this.setState({ 
-    count: this.state.count + 1,
-    score: this.state.score + 1 });
-
-//CHECK TO SEE IF COUNT < SCORE
-  //   if (this.state.count > this.state.score){
-  //     this.setState({score: this.state.count});
-  //   }
-  // }
-//CHECK TO SEE IF THE ID (from SaladCard) is in arrayCheck
-
+ //CHECK TO SEE IF THE ID (from SaladCard) is in arrayCheck
   //IF NOT PUSH IT INTO ARRAYCHECK AND ADD 1 to current score
-  //AND TEST IF COUNT > SCORE
       if (!this.state.arrayCheck.includes(id)){
           this.state.arrayCheck.push(id);
           console.log("AC: " + this.state.arrayCheck);
           this.setState({ count: this.state.count + 1 });
-          // if (this.state.count > this.state.score){
-          //   let y = this.state.count;
-          //   console.log("y: " + y);
-          //   this.setState({ score: y });
-          // }
+          this.shuffleArray(saladstuff);
         } 
-      
 
-
-
-//       let x = this.state.count;
-//       console.log("x: " + x);
-//       this.setState({ score: x });
-//     }
-//CHECK TO SEE IF THE ID (from SaladCard) is in arrayCheck
-        // console.log("id: " + id);
-//IF NOT PUSH IT INTO ARRAYCHECK AND ADD 1 to current score
-//AND TEST IF COUNT > SCORE
-  //   if (!this.state.arrayCheck.includes(id)){
-  //     this.state.arrayCheck.push(id);
-  //     console.log("AC: " + this.state.arrayCheck);
-  //     this.setState({ count: this.state.count + 1 });
-  //     if (this.state.count > this.state.score){
-  //       let y = this.state.count;
-  //       console.log("y: " + y);
-  //       this.setState({ score: y });
-  //     }
-  //   }
-  // } 
-// //IF THE ID IS IN arrayCHECK then set count=0 and empty array    
-//     else{
-//       //set arrayCheck to 0
-//       //set count to 0
-//       this.setState({ count: 0, arrayCheck: [] });
-//       console.log("AC: " + this.state.arrayCheck);
-//     }
-//   };
-
-  keepTrack = () => {
-
+//IF THE ID IS IN arrayCHECK then set count=0 and empty array    
+    else{
+      //set arrayCheck to 0, set count to 0, score stays
+      this.setState({ 
+        count: 0, 
+        arrayCheck: [],
+        score: this.state.score
+       });
+      console.log("AC: " + this.state.arrayCheck);
+    }
   }
-
-    // handleIncrement increments this.state.count by 1
-  handleLoss = () => {
-      //PSEUDO if number is in array, then handle loss. Do not pass functions down
-      // We always use the setState method to update a component's state
-      this.setState({ count: 0 });
-    };
-  
-
-
-
-  // removePoints = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   // const saladstuff = this.state.saladstuff.filter(saladstuff => saladstuff.id !== id);
-  //   const saladstuff = this.state.saladstuff.filter(saladstuff => saladstuff.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ saladstuff });
-  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -114,14 +68,16 @@ class App extends Component {
         ></Navbar>
         <InfoHeader></InfoHeader>
         
-        {this.state.saladstuff.map(saladstuff => (
-          <SaladCard
-            handleClick={this.handleClick}
-            key={saladstuff.id}
-            id={saladstuff.id}
-            image={saladstuff.image}
-          />
-        ))}
+          {this.state.saladstuff.map(saladstuff => (
+            <SaladCard
+              handleClick={this.handleClick}
+              shuffleArray={this.shuffleArray}
+              key={saladstuff.id}
+              id={saladstuff.id}
+              image={saladstuff.image}
+            />
+          ))}
+
       </Wrapper>
     );
   }
--- src/components/SaladCard/index.js
+++ src/components/SaladCard/index.js
@@ -3,14 +3,11 @@ import "./style.css";
 
 function SaladCard(props) {
   return (
-    <div onClick={ () => {props.handleClick(props.id)}} >
+   <div onClick={ () => {props.handleClick(props.id)}} >
       <div className="card">
         <div className="img-container">
           <img alt={props.name} src={props.image} />
         </div>
-        {/* <span onClick={() => props.removePoints(props.id)} className="remove">
-          𝘅
-        </span> */}
       </div>
     </div>
   );
