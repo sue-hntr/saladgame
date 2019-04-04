c8247df9f26b4c3b178554f2d12d6162ff2a3eb6
Suzanne Hunter <sue_huntr@yahoo.com>
2019-04-03 21:30:35 -0400

    Updates

---
+++ precache-manifest.fd913b97de86d2acaca54ed83e21a389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b99540d141dae98a06f",
+    "url": "/saladgame/static/css/main.3019fa23.chunk.css"
+  },
+  {
+    "revision": "2b99540d141dae98a06f",
+    "url": "/saladgame/static/js/main.d2257ec5.chunk.js"
+  },
+  {
+    "revision": "c94addeb37a301275e0d",
+    "url": "/saladgame/static/js/runtime~main.313f2a78.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/saladgame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "e27d49bda4f77ac0a82ef5b7b8c5ac49",
+    "url": "/saladgame/index.html"
+  }
+];
